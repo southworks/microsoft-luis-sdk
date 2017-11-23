e79d11856c2b05f0cdbf8894465d4a18c5fbb9d0
Enzo Cano <enzo.cano@southworks.com>
2017-11-23 18:22:12 -0300

    Luis prediction tests

---
+++ Runtime-SDK/.NET/LUIS-Runtime.Tests/BaseTest.cs
@@ -0,0 +1,23 @@
+using System.Net.Http;
+using Microsoft.Azure.CognitiveServices.Language.LUIS;
+using Microsoft.Azure.Test.HttpRecorder;
+using Microsoft.Azure.CognitiveServices.Language.LUIS.Models;
+
+namespace Microsoft.Azure.CognitiveServices.LUIS.Tests
+{
+    public abstract class BaseTest
+    {
+        protected ILuisRuntimeAPI GetClient(DelegatingHandler handler)
+        {
+            return new LuisRuntimeAPI(handlers: handler);
+        }
+
+        protected const HttpRecorderMode mode = HttpRecorderMode.Playback;
+
+        protected const AzureRegions region = AzureRegions.Westus;
+
+        protected const string appId = "f8ef55e6-1543-4a36-95da-497c21feea65";
+
+        protected const string subscriptionKey = "42176485494149818968315538ddf05b";
+    }
+}
---
+++ Runtime-SDK/.NET/LUIS-Runtime.Tests/Luis/PredictionTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Azure.CognitiveServices.Language.LUIS;
+using Microsoft.Azure.CognitiveServices.Language.LUIS.Models;
+using Microsoft.Azure.Test.HttpRecorder;
+using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
+using Xunit;
+
+namespace Microsoft.Azure.CognitiveServices.LUIS.Tests.Luis
+{
+    public class PredictionTests: BaseTest
+    {
+        private readonly string className;
+
+        public PredictionTests()
+        {
+            className = this.GetType().FullName;
+        }
+
+        [Fact]
+        public async void Prediction()
+        {
+            using (MockContext context = MockContext.Start(className))
+            {
+                HttpMockServer.Initialize(className, nameof(Prediction), mode);
+                ILuisRuntimeAPI client = GetClient(HttpMockServer.CreateInstance());
+                var utterance = "hello";
+                var result = await client.Prediction.GetPredictionsFromEndpointViaGetAsync(region, subscriptionKey, utterance, appId);
+
+                Assert.Equal("Family Intent", result.TopScoringIntent.Intent);
+                context.Stop();
+            }
+        }
+    }
+}
