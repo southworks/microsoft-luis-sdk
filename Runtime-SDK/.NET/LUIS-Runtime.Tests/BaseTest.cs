36b1450ed40951e54826ea6e79a38f88c9af7fd0
Enzo Cano <enzo.cano@southworks.com>
2017-11-23 18:43:01 -0300

    Tests Cleanup

--- Runtime-SDK/.NET/LUIS-Runtime.Tests/BaseTest.cs
+++ Runtime-SDK/.NET/LUIS-Runtime.Tests/BaseTest.cs
@@ -7,17 +7,16 @@ namespace Microsoft.Azure.CognitiveServices.LUIS.Tests
 {
     public abstract class BaseTest
     {
+        protected const HttpRecorderMode mode = HttpRecorderMode.Playback;
+        protected const AzureRegions region = AzureRegions.Westus;
+        protected const string appId = "f8ef55e6-1543-4a36-95da-497c21feea65";
+        protected const string subscriptionKey = "42176485494149818968315538ddf05b";
+
+        protected string ClassName => GetType().FullName;
+        
         protected ILuisRuntimeAPI GetClient(DelegatingHandler handler)
         {
             return new LuisRuntimeAPI(handlers: handler);
         }
-
-        protected const HttpRecorderMode mode = HttpRecorderMode.Playback;
-
-        protected const AzureRegions region = AzureRegions.Westus;
-
-        protected const string appId = "f8ef55e6-1543-4a36-95da-497c21feea65";
-
-        protected const string subscriptionKey = "42176485494149818968315538ddf05b";
     }
 }
--- Runtime-SDK/.NET/LUIS-Runtime.Tests/Luis/PredictionTests.cs
+++ Runtime-SDK/.NET/LUIS-Runtime.Tests/Luis/PredictionTests.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Azure.CognitiveServices.Language.LUIS;
-using Microsoft.Azure.CognitiveServices.Language.LUIS.Models;
 using Microsoft.Azure.Test.HttpRecorder;
 using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
 using Xunit;
@@ -8,20 +7,14 @@ namespace Microsoft.Azure.CognitiveServices.LUIS.Tests.Luis
 {
     public class PredictionTests: BaseTest
     {
-        private readonly string className;
-
-        public PredictionTests()
-        {
-            className = this.GetType().FullName;
-        }
-
         [Fact]
         public async void Prediction()
         {
-            using (MockContext context = MockContext.Start(className))
+            using (MockContext context = MockContext.Start(ClassName))
             {
-                HttpMockServer.Initialize(className, nameof(Prediction), mode);
+                HttpMockServer.Initialize(ClassName, nameof(Prediction), mode);
                 ILuisRuntimeAPI client = GetClient(HttpMockServer.CreateInstance());
+
                 var utterance = "hello";
                 var result = await client.Prediction.GetPredictionsFromEndpointViaGetAsync(region, subscriptionKey, utterance, appId);
 
