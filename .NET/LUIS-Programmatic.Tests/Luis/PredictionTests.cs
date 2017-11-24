599309e6351a511a27442d0d912399257df18b64
Enzo Cano <enzo.cano@southworks.com>
2017-11-24 11:33:03 -0300

    Add Programmatic tests (basic sample)

---
+++ .NET/LUIS-Programmatic.Tests/Luis/PredictionTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.CognitiveServices.Language.LUIS.Programmatic;
+using Xunit;
+
+namespace Microsoft.Azure.CognitiveServices.LUIS.Programmatic.Tests.Luis
+{
+    public class AppsTests: BaseTest
+    {
+        [Fact]
+        public void GetApplicationInfo()
+        {
+            UseClientFor(async client => {
+                var result = await client.Apps.GetApplicationInfoAsync(region, appId);
+
+                Assert.Equal(appId, result.Id);
+            });
+        }
+    }
+}
