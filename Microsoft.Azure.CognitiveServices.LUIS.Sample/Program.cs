bc9cb63bb9d212539e5b8939a2a9ed63c86a233f
pablo costantini <pablo.costantini@southworks.com>
2017-12-01 11:11:41 -0800

    Initial import
    - Simple Console example that use LUIS Runtime API to extract intent and entities from user input

---
+++ Microsoft.Azure.CognitiveServices.LUIS.Sample/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace Microsoft.Azure.CognitiveServices.Language.LUIS.Sample
+{
+    using System;
+    using System.IO;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Azure.CognitiveServices.Language.LUIS.Models;
+    using Newtonsoft.Json;
+
+    class Program
+    {
+        public static IConfigurationRoot Configuration { get; set; }
+
+        private static AzureRegions AzureRegion;
+        private static string SubscriptionKey;
+        private static string ApplicationId;
+
+        static void Main(string[] args)
+        {
+            ReadConfiguration();
+
+            // ShowIntro();
+
+            RecognizeUserInput().Wait();
+        }
+
+        static async Task RecognizeUserInput()
+        {
+            while (true)
+            {
+                // Read the text to recognize
+                Console.WriteLine("Enter the text to recognize:");
+                string input = Console.ReadLine().Trim();
+
+                if (input.ToLower() == "exit")
+                {
+                    // Close application if user types "exit"
+                    break;
+                }
+                else
+                {
+                    if (input.Length > 0)
+                    {
+                        // Create client with SuscriptionKey and AzureRegion
+                        var client = new LuisRuntimeAPI(new ApiKeyServiceClientCredentials(SubscriptionKey))
+                        {
+                            AzureRegion = AzureRegion
+                        };
+
+                        // Predict
+                        var result = await client.Prediction.ResolveAsync(ApplicationId, input);
+
+                        // Print result
+                        var json = JsonConvert.SerializeObject(result, Formatting.Indented);
+                        Console.WriteLine(json);
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+
+        static void ReadConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+
+            var region = Configuration["LUIS.Region"];
+            if (string.IsNullOrWhiteSpace(region))
+            {
+                throw new ArgumentException("Missing \"LUIS.Region\" in appsettings.json");
+            }
+
+            AzureRegion = (AzureRegions)Enum.Parse(typeof(AzureRegions), region, true);
+            SubscriptionKey = Configuration["LUIS.SubscriptionKey"];
+            ApplicationId = Configuration["LUIS.ApplicationId"];
+
+            if (string.IsNullOrWhiteSpace(SubscriptionKey))
+            {
+                throw new ArgumentException("Missing \"LUIS.SubscriptionKey\" in appsettings.json");
+            }
+
+            if (string.IsNullOrWhiteSpace(ApplicationId))
+            {
+                throw new ArgumentException("Missing \"LUIS.ApplicationId\" in appsettings.json");
+            }
+        }
+    }
+}
